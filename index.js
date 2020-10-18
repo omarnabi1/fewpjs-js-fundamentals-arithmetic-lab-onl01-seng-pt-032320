d2bafd9dee5a15435880f04ce04ed8edb2432ab3
omarnabi1 <onabi20@gmail.com>
2020-10-18 14:13:43 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+
+const num1 = 62;
+const num2 = 1;
+let multiply = num1 * num2
+
+
+let random = Math.floor(Math.random() * 10) + 10;
+
+
+let mod = 16%6;
+
+let max = Math.max(1,2,3,4,20)
